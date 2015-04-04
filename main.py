18369a9d610c08c14483f8b4412b22f30ce89dd5
Lingzi Zhuang <Lingzi@dyn-129-236-217-134.dyn.columbia.edu>
2015-04-04 19:10:45 -0400

    *

--- main.py
+++ main.py
@@ -82,7 +82,7 @@ def calculate_context_vectors(language):
 		for each_context in contexts:
 			for each_word in each_context:
 				if each_word not in s:
-				s.append(each_word)
+					s.append(each_word)
 
 		# Calculate context vectors with respect to s
 		for each_context in contexts:
