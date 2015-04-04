ca3fecdbdffe288367f1f00c4d1dd7d21e330dd7
Lingzi Zhuang <Lingzi@dyn-129-236-217-134.dyn.columbia.edu>
2015-04-04 19:09:52 -0400

    *

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import json
 import codecs
 import sys
 import unicodedata
+from sklearn import svm, neighbors
 import nltk
 import string
 
@@ -15,68 +16,82 @@ def replace_accented(input_str):
     nkfd_form = unicodedata.normalize('NFKD', input_str)
     return u"".join([c for c in nkfd_form if not unicodedata.combining(c)])
 
-def format_context(input_str):
+def format_str(input_str):
 	input_str = remove_punctuation(input_str)
 	input_str = replace_accented(input_str)
 	input_str = input_str.lower()
 	return input_str
 
-def calculate_context_vectors(input_file):
-	'''
-	Parse the .xml dev data file
+# def parse_data(input_file):
+# 	'''
+# 	Parse the .xml dev data file
 
-	param str input_file: The input data file path
-	return dict: A dictionary with the following structure
-		{
-			lexelt: [(instance_id, context), ...],
-			...
-		}
+# 	param str input_file: The input data file path
+# 	return dict: A dictionary with the following structure
+# 		{
+# 			lexelt: [(instance_id, context), ...],
+# 			...
+# 		}
+# 	'''
+# 	xmldoc = minidom.parse(input_file)
+# 	data = {}
+# 	lex_list = xmldoc.getElementsByTagName('lexelt')
+# 	for node in lex_list:
+# 		lexelt = format_str(node.getAttribute('item'))
+# 		data[lexelt] = []
+# 		inst_list = node.getElementsByTagName('instance')
+# 		for inst in inst_list:
+# 			instance_id = inst.getAttribute('id')
+# 			l = inst.getElementsByTagName('context')[0]
+# 			context = (l.childNodes[0].nodeValue + l.childNodes[1].firstChild.nodeValue + l.childNodes[2].nodeValue).replace('\n', '')
+# 			data[lexelt].append((instance_id, context))
+# 	return data
+
+def calculate_context_vectors(language):
 	'''
-	xmldoc = minidom.parse(input_file)
+	##############
+	'''
 	data = {} 
+	input_file = 'data/' + language + '-train.xml'
+	xmldoc = minidom.parse(input_file)
 	lex_list = xmldoc.getElementsByTagName('lexelt')
 	for node in lex_list:
 		all_context_words = []
-		lexelt = node.getAttribute('item')
+		lexelt = format_str(node.getAttribute('item'))
 		data[lexelt] = ()
-		sense_ctxt_dict = {}
+		sense_ids = []
+		contexts = []
 		inst_list = node.getElementsByTagName('instance')
 
 		for inst in inst_list:
 			#instance_id = inst.getAttribute('id')
 			sense_id = inst.getElementsByTagName('answer')[0].getAttribute('senseid')
 			l = inst.getElementsByTagName('context')[0]
-			left = nltk.word_tokenize(format_context(l.childNodes[0].nodeValue))
-			right = nltk.word_tokenize(format_context(l.childNodes[2].nodeValue.replace('\n', '')))
+			left = nltk.word_tokenize(format_str(l.childNodes[0].nodeValue))
+			right = nltk.word_tokenize(format_str(l.childNodes[2].nodeValue.replace('\n', '')))
 			left_k = left[-10:]
 			right_k = right[0:10]
 			context = []
 			context = left_k + right_k
-			# Append words in current context to all_context_words
-			all_context_words += context
 			
-			if sense_id in sense_ctxt_dict:
-				sense_ctxt_dict[sense_id] += context
-			else:
-				sense_ctxt_dict[sense_id] = context
+			sense_ids.append(sense_id)
+			contexts.append(context)
 		
 		# remove duplicate items in all_context_words to create s
 		s = []
-		for each_word in all_context_words:
+		for each_context in contexts:
+			for each_word in each_context:
 			if each_word not in s:
 				s.append(each_word)
 
 		# Calculate context vectors with respect to s
-		sense_ids = []
-		context_vectors = []
-		for a_sense_id, a_context in sense_ctxt_dict.iteritems():
+		for each_context in contexts:
 			# Initialize context vector: all zeros
 			context_vector = [0] * len(s)
-			for each_word in a_context:
+			for each_word in each_context:
 				idx = s.index(each_word)
 				context_vector[idx] += 1
 
-			sense_ids.append(a_sense_id)
 			context_vectors.append(context_vector)
 
 		data[lexelt] = (s, sense_ids, context_vectors)
